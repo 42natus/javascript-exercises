6080c50eee5f504fd8dbdee70b058021e630a3dc
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2024-12-04 18:32:50 +0100

    Add code to reverseString function
    Add actual code to reverseString function body.
    Change all "test.skip" in the spec file to just "test"; all test
    conditions are satisfied.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    stringToArray = string.split("");
 
+    let reversedString = "";
+
+    for (i = stringToArray.length-1; i >= 0; i--) {
+        reversedString = reversedString.concat(stringToArray[i]);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
