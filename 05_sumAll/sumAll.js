7d0441ed132a5bcabd1616957326852e432a0552
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2024-12-19 05:40:12 +0100

    Add code to sumAll function
    Add actual code to sumAll function body.
    Passed first 3 tests — will skip the rest for now.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,40 @@
-const sumAll = function() {
+const sumAll = function(beginInteger, endInteger) {
+    const allIntegers = [];
+    let sum = 0;
+    let i = 1;
 
+    let end, begin;
+
+    if (beginInteger > endInteger) {
+        end = beginInteger;
+        begin = endInteger;
+    } else {
+        begin = beginInteger;
+        end = endInteger;
+    }
+
+    allIntegers[0] = parseInt(begin);
+    
+    nextNumber = ++begin;
+
+    let control = true;
+
+    while (control) {
+        allIntegers[i] = nextNumber;
+
+        if (nextNumber == parseInt(end)) {
+            control = false;
+        }
+
+        nextNumber++;
+        i++;
+    }
+
+    for (let i = 0; i < allIntegers.length; i++) {
+        sum += allIntegers[i];
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
