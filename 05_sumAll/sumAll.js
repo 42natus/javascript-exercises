c2b6c3da4524f064d0d1c3b26097ffc115ed2c44
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2024-12-19 11:32:31 +0100

    refactor: remove redundant code
    Upon looking through TOP's solution, I realised a huge chunk of the
    function's code was unnecessary — removed that chunk.
    Also, edited some conditionals for readability & conciseness.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,38 +1,15 @@
 const sumAll = function(beginInteger, endInteger) {
-    const allIntegers = [];
     let sum = 0;
 
-    if (beginInteger < 0 || endInteger < 0) {
-        return "ERROR";
-    } else if (!(Number.isInteger(beginInteger) && Number.isInteger(endInteger))) {
-        return "ERROR";
-    }
+    if (beginInteger < 0 || endInteger < 0) return "ERROR"; 
+    if (!(Number.isInteger(beginInteger) && Number.isInteger(endInteger))) return "ERROR";
 
     // Work whether or not the larger number is the first argument
     let begin = (beginInteger < endInteger) ? beginInteger : endInteger;
     let end = (beginInteger < endInteger) ? endInteger : beginInteger;
 
-    allIntegers[0] = begin;
-    
-    nextNumber = ++begin;
-
-    let i = 1;
-
-    let control = true;
-
-    while (control) {
-        allIntegers[i] = nextNumber;
-        
-        if (nextNumber == end) {
-            control = false;
-        }
-
-        nextNumber++;
-        i++;
-    }
-
-    for (let i = 0; i < allIntegers.length; i++) {
-        sum += allIntegers[i];
+    for (let i = begin; i <= end; i++) {
+        sum += i;
     }
 
     return sum;
