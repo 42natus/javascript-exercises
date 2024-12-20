97ba243be36e1f8d0195b3a5c3410425d138574a
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2024-12-20 16:58:24 +0100

    refactor: alternative variable swap method
    To experiment, tried using the array destructuring syntax, rather than
    the conditional ternary operator to handle swapping the 'begin' and
    'end' variables depending on the bigger value.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,17 @@
 const sumAll = function(beginInteger, endInteger) {
+    let begin = beginInteger;
+    let end = endInteger;
+
+    if (beginInteger > endInteger) [begin, end] = [end, begin];
+    
     let sum = 0;
 
     if (beginInteger < 0 || endInteger < 0) return "ERROR"; 
     if (!(Number.isInteger(beginInteger) && Number.isInteger(endInteger))) return "ERROR";
 
     // Work whether or not the larger number is the first argument
-    let begin = (beginInteger < endInteger) ? beginInteger : endInteger;
-    let end = (beginInteger < endInteger) ? endInteger : beginInteger;
+    // let begin = (beginInteger < endInteger) ? beginInteger : endInteger;
+    // let end = (beginInteger < endInteger) ? endInteger : beginInteger;
 
     for (let i = begin; i <= end; i++) {
         sum += i;
