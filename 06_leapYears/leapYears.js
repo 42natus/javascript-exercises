bf65986f0ce53d6c2561d870914dfa035a7f0957
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2024-12-21 17:24:01 +0100

    refactor: add constants & simplify program logic
    Upon viewing TOP's solution, used constants to improve clarity.
    Removed unnecessary 'if' condition and return statements to aid
    conciseness.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,19 +1,9 @@
 const leapYears = function(year) {
-    if (year % 4 !== 0) {
-        return false;
-    } else if (year % 100 === 0) {
-        return (year % 400 === 0);
-    } else {
-        return true;
-    }
-
-    // let value = (year % 4 !== 0) 
-    //     ? false 
-    //     : (year % 100 === 0) 
-    //         ? (year % 400 === 0)  
-    //         : true;
-
-    // return value;
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+  
+    return (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred));
 };
 
 // Do not edit below this line
