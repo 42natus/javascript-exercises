813c658d6c7f678531b30f7e93c344377172fd76
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2024-12-22 15:45:54 +0100

    Add code to tempConversion function
    Add actual code to tempConversion function body.
    Change all "test.skip" in the spec file to just "test"; all test
    conditions are satisfied.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  const fahrenheitToCelsius = (fahrenheitTemp - 32) * (5/9);
+
+  const roundedFahrenheitToCelsius = Math.round(fahrenheitToCelsius * 10) / 10;
+
+  const result = parseFloat(roundedFahrenheitToCelsius.toFixed(1));
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const celsiusToFahrenheit = (celsiusTemp * (9/5) + 32);
+
+  const roundedCelsiusToFahrenheit = Math.round(celsiusToFahrenheit * 10) / 10;
+
+  const result = parseFloat(roundedCelsiusToFahrenheit.toFixed(1));
+
+  return result;
 };
 
 // Do not edit below this line
