090a798752ad25f324ad213eb758aa78bc1bd2d7
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-05 17:16:26 +0100

    Add function to check if character is alphanumeric

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const chars = str.split("");
+    const alphaNum = chars.filter(char => isAlphaNum(char));
 };
+
+function isAlphaNum(character) {
+    code = character.charCodeAt(0);
+    if (48 <= code && code <= 57 || // character is number
+        65 <= code && code <= 90 || // character is uppercase letter
+        97 <= code && code <= 122) { // character is lowercase letter
+        return true;
+    }
+    return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
