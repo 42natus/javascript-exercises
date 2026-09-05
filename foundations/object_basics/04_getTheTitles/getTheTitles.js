cb6b493ae4d0b475bcfd16085d4ceeb6532d1c03
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-05 18:55:14 +0100

    refactor: simplify logic

--- foundations/object_basics/04_getTheTitles/getTheTitles.js
+++ foundations/object_basics/04_getTheTitles/getTheTitles.js
@@ -1,8 +1,5 @@
 const getTheTitles = function(books) {
-    return books.reduce((titles, book) => {
-        titles.push(book.title)
-        return titles;
-    }, []);
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
