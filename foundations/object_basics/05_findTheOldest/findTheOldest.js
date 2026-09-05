d93b8ed8b177ccc978d0755b2b563dc39a66bea3
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-05 19:47:44 +0100

    Find oldest person in an array of people

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const today = (new Date().getFullYear());
+    
+    // sort people from oldest to youngest
+    let sortedPeople = people.slice().sort((personA, personB) => {
+        const ageA = (personA.yearOfDeath) ? personA.yearOfDeath - personA.yearOfBirth : today - personA.yearOfBirth;
 
+        const ageB = (personB.yearOfDeath) ? personB.yearOfDeath - personB.yearOfBirth : today - personB.yearOfBirth;
+
+        return ageB - ageA;
+    });
+
+    // return oldest person — from the front of the sorted array
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
