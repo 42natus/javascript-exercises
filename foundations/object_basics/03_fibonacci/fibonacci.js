bdc24c88a1d041df6e418e3b412ed83afa2b4c53
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-05 18:41:17 +0100

    refactor: improve readability

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,21 +1,16 @@
 const fibonacci = function(n) {
     n = parseInt(n);
 
-    if (n < 0) {
-        return "OOPS";
-    }
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
 
-    if (n === 0) {
-        return 0;
-    }
-
-    let a = 1;
-    let b = 1;
+    let first = 1;
+    let second = 1;
     for (let i = 2; i < n; i++) {
-        [a, b] = [b, a + b];
+        [first, second] = [second, first + second];
     }
 
-    return b;
+    return second;
 };
 
 // Do not edit below this line
