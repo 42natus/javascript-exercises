66133c9b9d100c50cb50b4dd0c06c74c48542974
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-05 20:24:19 +0100

    Add recreated solution to original solution
    Saw that TOP's alternative solution was actually what I'd initially
    done. Restored mine and appended my attempt at theirs to... show off (to
    myself, I guess).

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,8 +1,22 @@
-function getAge(birth, death) {
-    if (!death) {
-        death = (new Date()).getFullYear();
-    }
+const findTheOldest = function(people) {
+    const today = (new Date()).getFullYear();
     
+    // sort people from oldest to youngest
+    let sortedPeople = people.slice().sort((personA, personB) => {
+        const ageA = (personA.yearOfDeath) ? personA.yearOfDeath - personA.yearOfBirth : today - personA.yearOfBirth;
+        const ageB = (personB.yearOfDeath) ? personB.yearOfDeath - personB.yearOfBirth : today - personB.yearOfBirth;
+        return ageB - ageA;
+    });
+
+    // return oldest person — from the front of the sorted array
+    return sortedPeople[0];
+};
+
+/*
+// Recreated TOP solution: 
+
+function getAge(birth, death) {
+    death ??= (new Date()).getFullYear();    
     return death - birth;
 }
 
@@ -14,6 +28,7 @@ const findTheOldest = function(people) {
     });
 };
 
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
