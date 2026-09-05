3da78a752af2363e5baa306740f3734077a443e7
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-05 16:07:43 +0100

    Find the factorial of a number

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -18,8 +18,25 @@ const power = function(base, exponent) {
 	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const sumAll = function(start, stop) {
+    if (!Number.isInteger(start) || 
+        !Number.isInteger(stop) || 
+        start < 0 || 
+        stop < 0) {
+        return 'ERROR';
+    }
+
+    if (start > stop) [start, stop] = [stop, start];
+    range = Array(stop - start + 1).fill(start).map((val, index) => val + index);
+
+    return range.reduce((total, current) => total + current);
+};
+
+const factorial = function(num) {
+  // make a range of all numbers from 1 to num
+	range = Array(num).fill(num).map((num, index) => num - index);
+  
+  return (num === 0) ? 1 : range.reduce((factorial, number) => factorial * number);
 };
 
 // Do not edit below this line
