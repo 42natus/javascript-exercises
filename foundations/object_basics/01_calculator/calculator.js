8d2f158ac0eba1ce818dd227d0ca39af3bbd8518
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-05 16:34:29 +0100

    refactor: remove unused code; use simple logic

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -18,25 +18,12 @@ const power = function(base, exponent) {
 	return base ** exponent;
 };
 
-const sumAll = function(start, stop) {
-    if (!Number.isInteger(start) || 
-        !Number.isInteger(stop) || 
-        start < 0 || 
-        stop < 0) {
-        return 'ERROR';
-    }
-
-    if (start > stop) [start, stop] = [stop, start];
-    range = Array(stop - start + 1).fill(start).map((val, index) => val + index);
-
-    return range.reduce((total, current) => total + current);
-};
-
 const factorial = function(num) {
-  // make a range of all numbers from 1 to num
-	range = Array(num).fill(num).map((num, index) => num - index);
-  
-  return (num === 0) ? 1 : range.reduce((factorial, number) => factorial * number);
+  let factorial = 1;
+  for (let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
